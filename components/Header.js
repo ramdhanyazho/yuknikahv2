ed5633aa6452a68c915d4575e67741d0858cedee
Muhamad Ramdhany <muhamad_ramdhany@mitsuilease.co.id>
2025-09-13 09:32:01 +0700

    ganti ukuran 4

--- components/Header.js
+++ components/Header.js
@@ -12,10 +12,10 @@ export default function Header() {
            <Image 
             src="/logo-yuknikah.png"
             alt="yuknikah.id Logo"
-            width={100}
-            height={100}
+            width={150}
+            height={150}
             priority
-            style={{ width: 'auto', height: '100x' }} // Memastikan tinggi konsisten
+            style={{ width: 'auto', height: '55x' }} // Memastikan tinggi konsisten
           />
         </Navbar.Brand>
         <div className="d-flex d-lg-none">
