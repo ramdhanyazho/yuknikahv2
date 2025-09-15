c5d8c71dc5f4eadfed3955a42eb8388135534345
Muhamad Ramdhany <muhamad_ramdhany@mitsuilease.co.id>
2025-09-15 16:25:19 +0700

    Comit ke sekian kali

--- app/page.js
+++ app/page.js
@@ -1,16 +1,10 @@
 // app/page.js
 'use client';
 
-import { useSession } from 'next-auth/react';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 
-// Optional (disarankan untuk mencegah Vercel pre-render error)
-export const dynamic = 'force-dynamic';
-
 export default function HomePage() {
-  const { data: session } = useSession();
-
   return (
     <>
       <Header />
