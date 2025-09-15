2619fe69bdafbbc262392d1b47c65ca6560d9a6a
Muhamad Ramdhany <muhamad_ramdhany@mitsuilease.co.id>
2025-09-15 16:42:30 +0700

    Comit baru saja

--- app/page.js
+++ app/page.js
@@ -1,16 +1,27 @@
 // app/page.js
+
 'use client';
 
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
+import dynamic from 'next/dynamic';
+
+// Import komponen dengan SSR dimatikan agar tidak error di server
+const Header = dynamic(() => import('@/components/Header'), { ssr: false });
+const Hero = dynamic(() => import('@/components/Hero'), { ssr: false });
 
 export default function HomePage() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-      </main>
-    </>
-  );
+return (
+<>
+<Header />
+<main>
+<Hero />
+{/* Tambahkan komponen lain di sini seperti: /}
+{/ <Kategori /> /}
+{/ <Harga /> /}
+{/ <Fitur /> /}
+{/ <Testimoni /> /}
+{/ <FAQ /> /}
+{/ <Footer /> */}
+</main>
+</>
+);
 }
